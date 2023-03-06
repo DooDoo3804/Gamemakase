5bb6e26a409712280d27ca8e1472c1f1d6bba481
ebing <ebing.salmon@gmail.com>
2023-03-06 10:49:43 +0900

    💄 F.E feat : 상단바 수정중

--- front-end/src/styles/MainHeaderEmotion.js
+++ front-end/src/styles/MainHeaderEmotion.js
@@ -2,5 +2,8 @@ import styled from "@emotion/styled";
 import { Common } from "../styles/Common";
 
 export const MainHeaderWrapper = styled.div`
+  margin: 0;
   background-color: ${Common.colors.mainColor01};
+  color: ${Common.colors.lightGray01};
+  height: 3rem;
 `;
