e3f659ae0083052994a171d79501b2826d2775e5
ebing <ebing.salmon@gmail.com>
2023-03-06 14:59:47 +0900

    ✨ F.E feat : 홈 화면 배너 추가

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,5 +1,40 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Banner } from "../styles/HomeEmotion";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={0}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <Banner>
+            <div className="banner1">Slide 1</div>
+          </Banner>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Banner>
+            <div className="banner2">Slide 2</div>
+          </Banner>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 };
 
 export default Home;
--- front-end/src/styles/HomeEmotion.js
+++ front-end/src/styles/HomeEmotion.js
@@ -1,3 +1,20 @@
 import styled from "@emotion/styled";
+import { Common } from "./Common";
 
-export const Banner = styled.div``;
+export const Banner = styled.div`
+  height: 10rem;
+  color: ${Common.colors.lightGray01};
+  background: linear-gradient(
+    to bottom,
+    ${Common.colors.mainColor02},
+    ${Common.colors.lavender01}
+  );
+
+  .banner1 {
+    height: 100%;
+  }
+
+  .banner2 {
+    height: 100%;
+  }
+`;
