5312a599a45e469065c7d807e9bd0107c9ab24dc
unknown <dodo327@naver.com>
2023-03-08 17:47:37 +0900

    ✨Feat: 리뷰 데이터(빅) 전처리(수정)

--- game_data/get_reviews.py
+++ game_data/get_reviews.py
@@ -44,7 +44,10 @@ def get_file_name():
 def get_times():
     game_list = get_games()
     files_list = get_file_name()
+    total_reviews_list = []
+    exceptions = []
     c = 1
+    ec = 1
     for g in game_list:
         r_path = f"reviews\\reviews\\{g}"
         save_dir = ""
@@ -52,12 +55,15 @@ def get_times():
             time_data = []
             for f in files_list:
                 if f[1] == g:
-                    with open(f[0], 'r', encoding='utf8') as file:
+                    with open(f[0], 'r', encoding='UTF-8') as file:
                         datas = json.load(file)
-                    if datas["reviews"]["author"]["num_reviews"] < 10:
-                        continue
                     try:
+                        # 데이터 파싱
                         for r in datas["reviews"]:
+                            # 남긴 리뷰 10개 미만 거르기
+                            if r["author"]["num_reviews"] < 10:
+                                continue
+
                             fields = {
                                 "gameid": g,
                                 "steamid": r["author"]["steamid"],
@@ -68,22 +74,44 @@ def get_times():
                                 "models": 'reviews.review',
                                 "fields": fields
                             }
+                            # 게임 아이디 별로 저장
                             time_data.append(data)
+                            # 전체 게임을 하나의 파일로 저장
+                            total_reviews_list.append(data)
                             c += 1
-                            print(c)
+                            r_n = r["author"]["num_reviews"]
+                            print(f"{c} : {r_n}")
                         save_dir = f"final\\{f[1]}.json"
                     except:
                         print(f)
 
-            # print(save_dir)
-            with open(save_dir, 'w', encoding="utf") as make_file:
+            # 게임 아이디 별로 폴더 만들어 저장
+            with open(save_dir, 'w', encoding="UTF-8") as make_file:
                 json.dump(time_data, make_file,
                           ensure_ascii=False, indent="\t")
         else:
-            exceptions.append(g)
+            # 리뷰가 없는 예외인 경우
+            e_fields = {
+                "gameid": g,
+            }
+            e_data = {
+                "pk": c,
+                "models": 'reviews.exception',
+                "fields": e_fields
+            }
+            ec += 1
+            exceptions.append(e_data)
             print(f"{g} : don\'t have reviews!")
 
+    # 하나로 통합한 파일 생성
+    with open("total_reviews.json", 'w', encoding="UTF-8") as make_file:
+        json.dump(total_reviews_list, make_file,
+                  ensure_ascii=False, indent="\t")
 
-exceptions = []
+    # 예외 파일 생성
+    with open("exceptions.json", 'w', encoding="UTF-8") as make_file:
+        json.dump(exceptions, make_file,
+                  ensure_ascii=False, indent="\t")
+
+
 get_times()
-print(exceptions)
