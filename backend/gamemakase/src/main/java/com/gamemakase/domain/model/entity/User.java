a3c66c072fdb6565ee4a585c66167fa660839dde
SSAFY <xvsgxv@gmail.com>
2023-03-09 16:55:18 +0900

    :sparkles: feat: Spring Security + Json Web Token 연동 완료

--- backend/gamemakase/src/main/java/com/gamemakase/domain/model/entity/User.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/model/entity/User.java
@@ -6,15 +6,10 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -30,7 +25,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 @AllArgsConstructor
 @Builder
 @Table(name = "user")
-public class User implements UserDetails{
+public class User {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "user_id")
@@ -54,46 +49,11 @@ public class User implements UserDetails{
   @Column(name = "updated_at")
   private LocalDateTime updatedAt;
 
-  @ElementCollection(fetch = FetchType.EAGER)
-  @Builder.Default
-  private List<String> roles = new ArrayList<>();
-
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities(){
-    return this.roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-  }
-
-  @Override
-  public String getPassword() {
-    return this.userPassword;
-  }
-
-  @JsonProperty(access = Access.WRITE_ONLY)
-  @Override
-  public String getUsername(){
-    return this.userName;
-  }
-  @JsonProperty(access = Access.WRITE_ONLY)
-  @Override
-  public boolean isAccountNonExpired(){
-    return true;
-  }
-
-  @JsonProperty(access = Access.WRITE_ONLY)
-  @Override
-  public boolean isAccountNonLocked(){
-    return true;
-  }
-
-  @JsonProperty(access = Access.WRITE_ONLY)
-  @Override
-  public boolean isCredentialsNonExpired(){
-    return true;
-  }
-  @JsonProperty(access = Access.WRITE_ONLY)
-  @Override
-  public boolean isEnabled(){
-    return true;
-  }
+  @OneToOne
+  @JoinTable(
+          name = "user_authority_join",
+          joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "user_id")},
+          inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+  private Authority authority;
 
 }
--- backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/UserRepository.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/UserRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
   User findByUserName(String userName);
   User findByUserId(Long uid);
+  User findByUserEmail(String email);
+  User findByUserSteamId(long userSteamId);
+  User findByUserEmailAndUserPassword(String userEmail, String userPassword);
 }
