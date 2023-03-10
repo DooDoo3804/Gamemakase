d51f2d96b87aeb6bdcc6c5bd21a9d886c25d1434
ebing <ebing.salmon@gmail.com>
2023-03-10 16:30:31 +0900

    💄 F.E feat : 모달 기본 미디어쿼리 설정

--- front-end/src/components/ChatModal.js
+++ front-end/src/components/ChatModal.js
@@ -1,11 +1,38 @@
+import { useRef } from "react";
 import { ChatModalBody, ChatModalWrapper } from "../styles/ChatModalEmotion";
+import useBodyScrollLock from "./ScrollLock";
 
 const ChatModal = ({ chatView, setChatView, scrollPosition }) => {
+  const outSection = useRef();
+
+  const { openScroll } = useBodyScrollLock();
+
+  const handleClose = () => {
+    openScroll(scrollPosition);
+    setChatView(false);
+  };
+
   return (
     <>
       {chatView ? (
-        <ChatModalWrapper>
-          <ChatModalBody>ㅎㅇ</ChatModalBody>
+        <ChatModalWrapper
+          ref={outSection}
+          onClick={(e) => {
+            if (outSection.current === e.target) {
+              handleClose();
+            }
+          }}
+        >
+          <ChatModalBody
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+              transition: {},
+            }}
+          >
+            ㅎㅇ
+          </ChatModalBody>
         </ChatModalWrapper>
       ) : null}
     </>
--- front-end/src/styles/ChatModalEmotion.js
+++ front-end/src/styles/ChatModalEmotion.js
@@ -1,6 +1,54 @@
 import styled from "@emotion/styled";
 import { motion } from "framer-motion";
+import { Common } from "./Common";
 
-export const ChatModalWrapper = styled.div``;
+export const ChatModalWrapper = styled.div`
+  z-index: 999;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+`;
 
-export const ChatModalBody = styled(motion.div)``;
+export const ChatModalBody = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 80%;
+  min-height: 30rem;
+  background-color: ${Common.colors.mainColor01};
+  border: 2px solid ${Common.colors.white01};
+  transition: all 0.5s ease-in-out;
+
+  @media (min-width: 1024px) {
+    width: 60rem;
+    max-height: 50rem;
+    border-radius: 2rem;
+    padding: 2rem;
+  }
+  @media (max-width: 1024px) {
+    width: 45rem;
+    max-height: 40rem;
+    border-radius: 2rem;
+    padding: 1.8rem;
+  }
+  @media (max-width: 768px) {
+    width: 28rem;
+    max-height: 40rem;
+    border-radius: 1.5rem;
+    padding: 1.5rem;
+  }
+  @media (max-width: 500px) {
+    width: 85%;
+    height: 90%;
+    max-height: 50rem;
+    border-radius: 1rem;
+    padding: 1rem;
+  }
+`;
