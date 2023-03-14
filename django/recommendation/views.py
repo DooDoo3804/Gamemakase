ad6e39bb96e11daf6b130f3499b518ca4d3e082e
Bluuubery <zephyr72836@gmail.com>
2023-03-14 17:43:50 +0900

    ✨B.E feat: 게임 추천 뷰함수 진행중(db와 연결)
    Former-commit-id: 6d9f1ce43fcda9cd09104c60416a9377793331ff

--- django/recommendation/views.py
+++ django/recommendation/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+import pandas as pd
+import pymysql
+import pymysql.cursors
 
 # Create your views here.
+
+
+def get_recommended_games(request, user_id):
+    conn = pymysql.connect(
+        host="43.201.61.185",
+        user="root",
+        password="banapresso77",
+        db="gamemakase",
+        charset="utf8",
+        cursorclass=pymysql.cursors.DictCursor        
+    )
+    cursor = conn.cursor()
+    sql = "select * from gamemakase.rating"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    df = pd.DataFrame(result)
+    print(df.head(20))
+
+    # result = A(df, 1)
+    print(result)
+
+    pass
+
+
+# get_recommended_games(True, True)
