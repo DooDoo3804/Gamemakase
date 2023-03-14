dc378ed9cb523ac7d28ffd2a21cecc4f18eeb40e
unknown <dodo327@naver.com>
2023-03-14 13:24:00 +0900

    👷django my_settings 수정
    Former-commit-id: c586723639b0e7994bde45c5165d0059695d9157

--- django/config/my_settings.py
+++ django/config/my_settings.py
@@ -3,8 +3,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "gamemakase",
         "USER": "root",
-        "PASSWORD": "ssafy",
-        "HOST": "localhost",
+        "PASSWORD": "banapresso77",
+        "HOST": "http://j8a106.p.ssafy.io",
         "PORT": '3306'
     }
 }
