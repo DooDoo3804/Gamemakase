ee4f8db5561fe782d5dcc6be74dc2b9af25fc34c
ebing <ebing.salmon@gmail.com>
2023-03-15 10:54:24 +0900

    ✨ F.E feat : 로그인 모달 내부 컴포넌트 추가
    남은 부분은 로그인 관련 코드 생기면 진행할 예정
    
    S08P22A106-63

--- front-end/src/components/LoginModal.js
+++ front-end/src/components/LoginModal.js
@@ -5,6 +5,8 @@ import useBodyScrollLock from "./ScrollLock";
 import { Common } from "../styles/Common";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import gamemakase_logo from "../assets/gamemakase_logo.svg";
+import { faSteam } from "@fortawesome/free-brands-svg-icons";
 
 const LoginModalWrapper = styled(motion.div)`
   z-index: 999;
@@ -24,7 +26,7 @@ const LoginModalBody = styled(motion.div)`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 70%;
+  height: 60%;
   background-image: linear-gradient(
     to bottom,
     rgba(50, 50, 50, 1),
@@ -33,25 +35,26 @@ const LoginModalBody = styled(motion.div)`
   border: 2px solid ${Common.colors.white01};
   color: ${Common.colors.white01};
   transition: all 0.5s ease-in-out;
+  font-family: "Sarpanch", sans-serif;
 
   @media (min-width: 1024px) {
-    width: 30rem;
-    min-height: 35rem;
-    max-height: 40rem;
+    width: 25rem;
+    min-height: 25rem;
+    max-height: 30rem;
     border-radius: 2rem;
     padding: 2rem;
   }
   @media (max-width: 1024px) {
-    width: 25rem;
-    min-height: 35rem;
-    max-height: 40rem;
+    width: 22rem;
+    min-height: 25rem;
+    max-height: 30rem;
     border-radius: 2rem;
     padding: 1.8rem;
   }
   @media (max-width: 768px) {
     width: 20rem;
-    min-height: 30rem;
-    max-height: 40rem;
+    min-height: 25rem;
+    max-height: 30rem;
     border-radius: 1.5rem;
     padding: 1.5rem;
   }
@@ -82,6 +85,38 @@ const LoginModalBody = styled(motion.div)`
     .x-mark {
       cursor: pointer;
     }
+  }
+
+  .login-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 80%;
+  }
+
+  .logo-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .logo {
+  }
+
+  .welcome-text {
+  }
+
+  .login-btn {
+    font-weight: 700;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${Common.colors.mainColor06};
+    width: 10rem;
+    height: 3rem;
+    border-radius: 2rem;
+    cursor: pointer;
   }
 `;
 
@@ -124,6 +159,17 @@ const LoginModal = ({ loginView, setLoginView, scrollPosition }) => {
                 onClick={() => handleClose()}
                 className="x-mark"
               />
+            </div>
+            <div className="login-wrapper">
+              <div className="logo-wrapper">
+                <img src={gamemakase_logo} alt="logo" className="logo" />
+                <p className="welcome-text">Welcome to GameMakase !</p>
+              </div>
+              <div className="login-btn">
+                <p className="login-text">
+                  SIGN IN <FontAwesomeIcon icon={faSteam} />
+                </p>
+              </div>
             </div>
           </LoginModalBody>
         </LoginModalWrapper>
