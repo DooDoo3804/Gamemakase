e401471e238916f0f05842e6f0155f01d343f32c
dodo327 <dodo327@naver.com>
2023-03-20 15:25:28 +0900

    🚑 B.E fix : 일부 수정
    Former-commit-id: d45b27b1b39b19caa3b35d5a0f845cc3665eb324

--- django/recommendation/updator.py
+++ django/recommendation/updator.py
@@ -5,7 +5,8 @@ from django_apscheduler.jobstores import register_events, DjangoJobStore
 def start():
     print("start schedule")
     scheduler = BackgroundScheduler()
-    scheduler.add_job(schedule_api, 'cron', minute = '*/5', second = '0')
+    # 매 정각 실행
+    scheduler.add_job(schedule_api, 'cron', minute = '0', second = '0')
     print("add schedule")
     register_events(scheduler)
     # @scheduler.scheduled_job('cron', minute = '*/5', second = '0', name = 'auto_update')
