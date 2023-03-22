a77c30b0b16b4b455b16125a85b37fda743a0a93
ebing <ebing.salmon@gmail.com>
2023-03-22 16:34:34 +0900

    ✨ F.E feat : 홈 화면 게임 임시 API 연결
    S08P22A106-124

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,9 +1,10 @@
 import { useNavigate } from "react-router";
-import { useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, A11y, Autoplay } from "swiper";
 import Lottie from "react-lottie";
+import axios from "axios";
 import {
   HomeWrapper,
   Banner,
@@ -19,13 +20,22 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import GameCarousel from "../components/GameCarousel";
 import GameClip from "../components/GameClip";
+import { BACKEND_URL } from "../config";
 
 const Home = () => {
   const navigate = useNavigate();
+  const [recommendGames, setRecommendGames] = useState([]);
+  const [popularGames, setPopularGames] = useState([]);
+  // const [moreGames, setMoreGames] = useState([]);
   const [hasNextPage, setHasNextPage] = useState(true);
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const swiperRef = useRef(null);
+  const popularNo = useRef(0);
+  const recommendNo = useRef(0);
   const [ref, inView] = useInView();
+
+  const size = 20;
+  const userId = 1;
 
   // 임시 데이터
   const todayGames = [
@@ -61,71 +71,38 @@ const Home = () => {
     },
   ];
 
-  const recommendGames = [
-    {
-      gameId: 10,
-      gameName: "Stray",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1332010/header_292x136.jpg?t=1670349423",
-    },
-    {
-      gameId: 11,
-      gameName: "Cult of the Lamb",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1313140/header_292x136.jpg?t=1674826230",
-    },
-    {
-      gameId: 3,
-      gameName: "Help Me!",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1557780/header_292x136_koreana.jpg?t=1640234732",
-    },
-    {
-      gameId: 4,
-      gameName: "Call of Duty",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1767320/header_292x136_koreana.jpg?t=1642579277",
-    },
-    {
-      gameId: 5,
-      gameName: "The Past Within",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1515210/header_292x136.jpg?t=1676931955",
-    },
-  ];
-
-  const popularGames = [
-    {
-      gameId: 10,
-      gameName: "Stray",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1332010/header_292x136.jpg?t=1670349423",
-    },
-    {
-      gameId: 2,
-      gameName: "Cult of the Lamb",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1313140/header_292x136.jpg?t=1674826230",
-    },
-    {
-      gameId: 3,
-      gameName: "Help Me!",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1557780/header_292x136_koreana.jpg?t=1640234732",
-    },
-    {
-      gameId: 4,
-      gameName: "Call of Duty",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1767320/header_292x136_koreana.jpg?t=1642579277",
-    },
-    {
-      gameId: 5,
-      gameName: "The Past Within",
-      gameImage:
-        "https://cdn.akamai.steamstatic.com/steam/apps/1515210/header_292x136.jpg?t=1676931955",
-    },
-  ];
+  // const popularGames = [
+  //   {
+  //     gameId: 10,
+  //     gameName: "Stray",
+  //     gameImage:
+  //       "https://cdn.akamai.steamstatic.com/steam/apps/1332010/header_292x136.jpg?t=1670349423",
+  //   },
+  //   {
+  //     gameId: 2,
+  //     gameName: "Cult of the Lamb",
+  //     gameImage:
+  //       "https://cdn.akamai.steamstatic.com/steam/apps/1313140/header_292x136.jpg?t=1674826230",
+  //   },
+  //   {
+  //     gameId: 3,
+  //     gameName: "Help Me!",
+  //     gameImage:
+  //       "https://cdn.akamai.steamstatic.com/steam/apps/1557780/header_292x136_koreana.jpg?t=1640234732",
+  //   },
+  //   {
+  //     gameId: 4,
+  //     gameName: "Call of Duty",
+  //     gameImage:
+  //       "https://cdn.akamai.steamstatic.com/steam/apps/1767320/header_292x136_koreana.jpg?t=1642579277",
+  //   },
+  //   {
+  //     gameId: 5,
+  //     gameName: "The Past Within",
+  //     gameImage:
+  //       "https://cdn.akamai.steamstatic.com/steam/apps/1515210/header_292x136.jpg?t=1676931955",
+  //   },
+  // ];
 
   const moreGames = [
     {
@@ -159,6 +136,74 @@ const Home = () => {
         "https://cdn.akamai.steamstatic.com/steam/apps/1515210/header_292x136.jpg?t=1676931955",
     },
   ];
+
+  useEffect(() => {
+    // todo : 로그인했을 때만 실행하도록 수정
+    // todo : 로그인 기능 만들어지면 api 변경
+    axios
+      .get(
+        `${BACKEND_URL}api/recommend/games/${userId}?page=${recommendNo.current}&size=${size}`,
+        {
+          headers: { "Content-Type": "application/json", userId: 1 },
+        }
+      )
+      .then(function (response) {
+        // console.log(response.data);
+        setRecommendGames(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        if (error.response.status === 500) {
+          window.location.replace("/500");
+        } else {
+        }
+      });
+    axios
+      .get(
+        `${BACKEND_URL}api/recommend/games/${userId}?page=${popularNo.current}&size=${size}`,
+        {
+          headers: { "Content-Type": "application/json", userId: 1 },
+        }
+      )
+      .then(function (response) {
+        // console.log(response.data);
+        setPopularGames(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+        if (error.response.status === 500) {
+          window.location.replace("/500");
+        } else {
+        }
+      });
+  }, []);
+
+  // const getMoreGames = useCallback(async () => {
+  //   setIsLoading(true);
+
+  //   await axios
+  //     .get(
+  //       `${BACKEND_URL}api/recommend/games/${userId}?page=${popularNo.current}&size=${size}`,
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     )
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       if (response.data.length) {
+  //         popularNo.current += 1;
+  //       }
+  //       set((reviewData) => [...reviewData, ...response.data]);
+  //       setHasNextPage(response.data.length === 12);
+  //       setReviewLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       console.log(error);
+  //       setReviewLoading(false);
+  //     });
+  // });
 
   const options = (lottiefile) => {
     return {
