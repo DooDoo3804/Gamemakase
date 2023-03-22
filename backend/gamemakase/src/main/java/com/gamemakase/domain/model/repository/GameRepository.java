2e0ade0b04ccc6a5bf8ca6b5f810c72cff8d9497
kwakminju <kacamimi@naver.com>
2023-03-22 16:25:45 +0900

    ✨B.E feat: user Info를 실시간 반영으로 변경, 검색 부분 메인 api 구현

--- backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/GameRepository.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/GameRepository.java
@@ -1,13 +1,14 @@
 package com.gamemakase.domain.model.repository;
 
 import com.gamemakase.domain.model.entity.Game;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-
     Optional<Game> findByGameId(Long gameId);
-
-
+    Page<Game> findAllByGameNameLikeOrderByGameName(String gameName, Pageable pageable);
 }
