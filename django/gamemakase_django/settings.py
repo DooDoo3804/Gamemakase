ae5f244bfe25d1cd8cd0a485944c90cea2b70d48
dodo327 <dodo327@naver.com>
2023-03-23 15:08:52 +0900

    📝 B.E fix : allowed host 추가
    Former-commit-id: 51fa5b7259ec16c0fa3713476bcb0b0f5536672d

--- django/gamemakase_django/settings.py
+++ django/gamemakase_django/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['43.201.61.185', 'gamemakase.com']
+ALLOWED_HOSTS = ['43.201.61.185', 'gamemakase.com', 'www.gamemakase.com']
 # ALLOWED_HOSTS = ['*']
 
 
