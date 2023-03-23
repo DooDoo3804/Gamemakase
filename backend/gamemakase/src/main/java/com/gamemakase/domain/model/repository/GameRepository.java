2ff9bc331775762e9b44581a770022d39ba5eec7
Bluuubery <zephyr72836@gmail.com>
2023-03-23 12:36:24 +0900

    ✨B.E feat: 인기게임 메서드 추가

--- backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/GameRepository.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/GameRepository.java
@@ -3,11 +3,13 @@ package com.gamemakase.domain.model.repository;
 import com.gamemakase.domain.model.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
     Optional<Game> findByGameId(Long gameId);
+    List<Game> findTop100ByOrderByPeakCcuDesc();
 
 
 }
