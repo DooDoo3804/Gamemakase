5ed9c16c2bbefba326ea04d3c02f13899d7dbd13
ebing <ebing.salmon@gmail.com>
2023-03-24 10:56:34 +0900

    ✨ F.E feat : info 아이콘 컴포넌트 기본 틀 추가

--- front-end/src/components/InfoIcon.js
+++ front-end/src/components/InfoIcon.js
@@ -1,10 +1,50 @@
+import styled from "@emotion/styled";
+import { AnimatePresence, motion } from "framer-motion";
 import infoSVG from "../assets/fontAwesomeSvg/circle-info-solid.svg";
 
-const InfoIcon = () => {
+const InfoIconWrapper = styled.div`
+  display: flex;
+  width: 1rem;
+  height: 1rem;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+
+  .box-body {
+    position: absolute;
+    z-index: 9;
+    padding: 2rem;
+    background-color: rgba(255, 255, 255, 0.4);
+
+    font-size: 1rem;
+    font-weight: 500;
+  }
+`;
+
+const InfoIcon = ({ text }) => {
   return (
-    <div>
-      <img src={infoSVG} alt />
-    </div>
+    <AnimatePresence>
+      <InfoIconWrapper>
+        <img src={infoSVG} alt="info-icon" />
+        <motion.div
+          className="box-body"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+          }}
+          exit={{
+            opacity: 0,
+            y: 50,
+          }}
+        >
+          {text}
+        </motion.div>
+      </InfoIconWrapper>
+    </AnimatePresence>
   );
 };
 
