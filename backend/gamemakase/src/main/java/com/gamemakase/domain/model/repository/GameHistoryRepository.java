cceb02c8546199684b387234d4b18702c831ca00
Bluuubery <zephyr72836@gmail.com>
2023-03-24 18:02:48 +0900

    🚑B.E fix: 게임 디테일 유저 추천 로직 변경
    추천 테이블에서 가져오던 걸 게임 플레이 기록에서 가져오는 걸로 바꿨습니다.

--- backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/GameHistoryRepository.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/GameHistoryRepository.java
@@ -6,10 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gamemakase.domain.model.entity.GameHistory;
 import com.gamemakase.domain.model.entity.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GameHistoryRepository extends JpaRepository<GameHistory, Long>{
 	List<GameHistory> findAllByUser(User user);
 
     boolean existsByGameGameIdAndUserUserId(Long gameId, Long userId);
+
+    @Query("SELECT gh FROM GameHistory gh JOIN FETCH gh.user u WHERE gh.game.gameId = :gameId ORDER BY gh.totalPlayGame DESC")
+    List<GameHistory> findAllByGameGameIdOrderByTotalPlayGameDesc(@Param("gameId") Long gameId);
+
+
 
 }
