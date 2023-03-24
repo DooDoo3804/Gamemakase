4f8e4cef05ee230b2226bdc6655b155dda446192
Bluuubery <zephyr72836@gmail.com>
2023-03-24 16:50:03 +0900

    ✨B.E feat: 랜덤게임 레포지토리 메서드 작성

--- backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/GameRepository.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/GameRepository.java
@@ -1,6 +1,6 @@
 package com.gamemakase.domain.model.repository;
 
-import com.gamemakase.domain.model.dto.PopularGameResponseDto;
+import com.gamemakase.domain.model.dto.GameResponseDto;
 import com.gamemakase.domain.model.entity.Game;
 
 import org.springframework.data.domain.Page;
@@ -22,10 +22,24 @@ public interface GameRepository extends JpaRepository<Game, Long> {
 
     List<Game> findTop100ByOrderByPeakCcuDesc();
 
-    @Query("SELECT new com.gamemakase.domain.model.dto.PopularGameResponseDto(g.gameId, g.gameName, i.imagePath) " +
+    @Query("SELECT new com.gamemakase.domain.model.dto.GameResponseDto(g.gameId, g.gameName, i.imagePath) " +
             "FROM Game g " +
-            "LEFT JOIN Image i ON g.gameId = i.typeId WHERE i.type = 'GAME_HEADER' AND g.peakCcu IS NOT NULL ORDER BY g.peakCcu DESC")
-    List<PopularGameResponseDto> findTop100GamesWithImagesOrderByPeakCcuDesc();
+            "LEFT JOIN Image i ON g.gameId = i.typeId " +
+            "WHERE i.type = 'GAME_HEADER' AND g.peakCcu IS NOT NULL " +
+            "ORDER BY g.peakCcu DESC")
+    Page<GameResponseDto> findTop100GamesWithImagesOrderByPeakCcuDesc(Pageable pageable);
+
+    @Query("SELECT new com.gamemakase.domain.model.dto.GameResponseDto(g.gameId, g.gameName, i.imagePath) " +
+            "FROM Game g " +
+            "LEFT JOIN Image i ON g.gameId = i.typeId " +
+            "WHERE i.type = 'GAME_HEADER' AND g.peakCcu IS NOT NULL " +
+            "AND (g.score >= 95 or g.recommendations >= 50000)" +
+            "ORDER BY RAND()")
+    Page<GameResponseDto> getAllByScoreGreaterThanEqual95OrRecommendationsGreaterThanEqual50000(Pageable pageable);
+
+
+
+
 
 
 
