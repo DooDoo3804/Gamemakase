a61a017234a8fbec0dcb09caf8c2d60c724896ff
ebing <ebing.salmon@gmail.com>
2023-03-24 10:30:27 +0900

    ✨ F.E feat : info 아이콘 컴포넌트 추가

---
+++ front-end/src/components/InfoIcon.js
@@ -0,0 +1,11 @@
+import infoSVG from "../assets/fontAwesomeSvg/circle-info-solid.svg";
+
+const InfoIcon = () => {
+  return (
+    <div>
+      <img src={infoSVG} alt />
+    </div>
+  );
+};
+
+export default InfoIcon;
