f41d54b42c55bf8e7a4617e6709bebbc69e17645
ebing <ebing.salmon@gmail.com>
2023-03-28 13:25:24 +0900

    ✨ F.E feat : 로그인 페이지 내부 필요한 요청들 추가하는 중

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,35 +1,109 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
+import Lottie from "react-lottie";
 
 import { useRecoilState } from "recoil";
 import { userState } from "../recoil/user";
 
-import LoadingPage from "../components/LoadingPage";
 import { BACKEND_URL } from "../config";
+import LoadingPage from "../components/LoadingPage";
+import { LoadingWrapper } from "../components/LoadingPage";
+
+import calc_loading from "../assets/lottie/calc-loading.json";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
   const [cookies, setCookies] = useCookies(["accessToken"]);
   const [user, setUser] = useRecoilState(userState);
 
-  useEffect(() => {
-    console.log(cookies["accessToken"]);
+  // 임시 데이터
+  const userId = 1;
 
-    axios
-      .get(`${BACKEND_URL}auth/user`, {
+  // useEffect(() => {
+  //   console.log(cookies["accessToken"]);
+
+  //   axios
+  //     .get(`${BACKEND_URL}auth/user`, {
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         accessToken: cookies["accessToken"],
+  //       },
+  //     })
+  //     .then(function (response) {
+  //       console.log(response.data);
+  //       // setUser(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    handleLogin();
+  }, []);
+
+  const handleLogin = async () => {
+    try {
+      // console.log(cookies["accessToken"]);
+      const loginResponse = await axios.get(`${BACKEND_URL}auth/user`, {
         headers: {
           "Content-Type": "application/json",
           accessToken: cookies["accessToken"],
         },
-      })
-      .then(function (response) {
-        console.log(response.data);
-        // setUser(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
       });
-  }, []);
+      setUser(loginResponse.data);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // todo : 가입시에만 하도록 처리
+    try {
+      console.log("계산 요청을 시작합니다.");
+      setLoading(true);
+      const calcResponse = await axios.get(
+        `${BACKEND_URL}auth/recommend/users/small/${userId}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            accessToken: cookies["accessToken"],
+          },
+        }
+      );
+      console.log(calcResponse.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const options = (lottiefile) => {
+    return {
+      loop: true,
+      autoplay: true,
+      animationData: lottiefile,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice",
+      },
+    };
+  };
+
+  if (loading) {
+    return (
+      <div>
+        <LoadingWrapper>
+          <Lottie
+            options={options(calc_loading)}
+            height={300}
+            width={300}
+          ></Lottie>
+          <p> </p>
+          <p className="title-text">사용자의 게임 기록을 분석 중입니다.</p>
+          <p className="info-text">잠시만 기다려주세요.</p>
+        </LoadingWrapper>
+      </div>
+    );
+  }
 
   return (
     <div>
