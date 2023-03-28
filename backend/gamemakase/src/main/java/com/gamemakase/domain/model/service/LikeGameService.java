5ccfa5bb0f59b02cde714735f6f5fb4ef901ee6b
Bluuubery <zephyr72836@gmail.com>
2023-03-28 15:00:22 +0900

    ✨ B.E feat : likegame 서비스 작성

---
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/model/service/LikeGameService.java
@@ -0,0 +1,8 @@
+package com.gamemakase.domain.model.service;
+
+import com.gamemakase.global.Exception.NotFoundException;
+import com.gamemakase.global.Exception.TokenValidFailedException;
+
+public interface LikeGameService {
+    Long insertLikeGame(Long gameId, String token) throws NotFoundException, TokenValidFailedException;
+}
---
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/model/service/LikeGameServiceImpl.java
@@ -0,0 +1,55 @@
+package com.gamemakase.domain.model.service;
+
+import com.gamemakase.domain.model.entity.Game;
+import com.gamemakase.domain.model.entity.LikeGame;
+import com.gamemakase.domain.model.entity.User;
+import com.gamemakase.domain.model.repository.GameRepository;
+import com.gamemakase.domain.model.repository.LikeGameRepository;
+import com.gamemakase.domain.model.repository.UserRepository;
+import com.gamemakase.global.Exception.NotFoundException;
+import com.gamemakase.global.Exception.TokenValidFailedException;
+import com.gamemakase.global.config.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class LikeGameServiceImpl implements LikeGameService{
+
+    private final Logger logger = LoggerFactory.getLogger(GameServiceImpl.class);
+    private final GameRepository gameRepository;
+    private final UserRepository userRepository;
+    private final LikeGameRepository likeGameRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
+
+    @Override
+    public Long insertLikeGame(Long gameId, String token) throws NotFoundException, TokenValidFailedException {
+        Game game = gameRepository.findByGameId(gameId)
+                .orElseThrow(() -> new NotFoundException("wrong game id"));
+
+        //        유저 파싱 및 예외처리
+        String userIdStr = jwtTokenProvider.getUserId(token);
+        Long userId = Long.parseLong(userIdStr);
+        User user;
+
+        try {
+            user = userRepository.findById(userId)
+                    .orElseThrow(() -> new NotFoundException("wrong userId"));
+        } catch (NumberFormatException e) {
+            logger.error(e.getMessage());
+            throw new TokenValidFailedException("유효하지 않은 토큰값입니다.");
+        }
+
+        return likeGameRepository.save(LikeGame.builder()
+                .game(game)
+                .user(user)
+                .type("game")
+                .build()).getLikeId();
+    }
+}
