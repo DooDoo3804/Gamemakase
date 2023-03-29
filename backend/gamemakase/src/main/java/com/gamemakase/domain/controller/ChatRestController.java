596fe4a1946c96a3545d83e1f7032b0da922b4ee
정진수 <zxcv423@naver.com>
2023-03-29 13:10:37 +0900

    :bug: fix : 채탕 불러오기 및 accessToken 유효시간 24시간으로 증가

--- backend/gamemakase/src/main/java/com/gamemakase/domain/controller/ChatRestController.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/controller/ChatRestController.java
@@ -5,10 +5,14 @@ import com.gamemakase.domain.model.dto.ChatListResponse;
 import com.gamemakase.domain.model.entity.MongoChat;
 import com.gamemakase.domain.model.repository.ChatRepository;
 import com.gamemakase.domain.model.service.ChatService;
+import com.gamemakase.domain.model.service.UserServiceImpl;
+import com.gamemakase.global.config.jwt.JwtTokenProvider;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -16,6 +20,7 @@ import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -26,26 +31,34 @@ public class ChatRestController {
   private final ChatService chatService;
   private final SimpMessageSendingOperations sendingOperations;
 
+  private final JwtTokenProvider jwtTokenProvider;
+
+  private final Logger logger = LoggerFactory.getLogger(ChatRestController.class);
+
   //front와 연동할 때는 인자 값에 @RequestBody 어노테이션 추가
   @MessageMapping("/send")
   public ResponseEntity<ChatInsertRequestDto> sendChat(@RequestBody ChatInsertRequestDto requestDto) throws ClassNotFoundException{
 
     System.out.println("message보내기 성공");
+    System.out.println("gameId : " + requestDto.getGameId());
+    System.out.println("chatRoomId : " + requestDto.getChatRoomId());
+
     chatService.insertChat(requestDto);
 
-    sendingOperations.convertAndSend("/sub/chat/" +requestDto.getGameId() + requestDto.getChatRoomId(), requestDto);
+    sendingOperations.convertAndSend("/sub/chat/" + requestDto.getChatRoomId(), requestDto);
     return ResponseEntity.status(HttpStatus.CREATED).body(requestDto);
   }
 
-  @GetMapping(value = "/chat/{gameId}/{chatRoomId}")
-  public ResponseEntity<List<ChatListResponse>> getChatByChatRoomNum(@PathVariable Long gameId, @PathVariable Long chatRoomId) throws ClassNotFoundException{
-    System.out.println("조회 성공");
+  @GetMapping(value = "/auth/chatroom/{gameId}/{chatRoomId}")
+  public ResponseEntity<List<ChatListResponse>> getChatByChatRoomNum(@RequestHeader(value = "accessToken") String token, @PathVariable("gameId") long gameId, @PathVariable("chatRoomId") Long chatRoomId){
+    logger.info("채팅 조회 성공");
 
+    String userIdStr = jwtTokenProvider.getUserId(token);
 
-    List<MongoChat> chatList = chatService.findByRoomNumOrderByCreatedAtDesc(chatRoomId);
+    long writerId = Long.parseLong(userIdStr);
 
-//    List<ChatListResponse> chatResList = chatList.stream().map(ChatListResponse::new).collect(
-//        Collectors.toList());
+    List<MongoChat> chatList = chatService.findByGameIdAndChatRoomId(gameId, chatRoomId);
+
 
     List<ChatListResponse> chatResList = chatList.stream().map(ChatListResponse::new).collect(
         Collectors.toList());
