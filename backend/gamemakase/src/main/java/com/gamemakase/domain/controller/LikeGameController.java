5e71fbea85958630f03b5b339d8fd481fed87e59
Bluuubery <zephyr72836@gmail.com>
2023-03-29 13:39:31 +0900

    ✨ B.E feat : likegame 삭제 컨트롤러 작성

--- backend/gamemakase/src/main/java/com/gamemakase/domain/controller/LikeGameController.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/controller/LikeGameController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
@@ -28,11 +25,24 @@ public class LikeGameController {
     ResponseEntity<HashMap<String, Long>> insertLikeGame(
             @RequestHeader("accessToken") @ApiParam(required = true) String token,
             @PathVariable Long gameId) throws NotFoundException, TokenValidFailedException {
+
         Long likeGame = likeGameService.insertLikeGame(gameId, token);
         HashMap<String, Long> result = new HashMap<>();
         result.put("likeId",likeGame);
 
-        return ResponseEntity.status(HttpStatus.OK).body(result);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
 
     }
+
+    @DeleteMapping("/auth/user/bookmarks/{likeId}")
+    ResponseEntity<?> deleteLikeGame(
+            @RequestHeader("accessToken") @ApiParam(required = true) String token,
+            @PathVariable Long likeId) throws NotFoundException, TokenValidFailedException {
+
+        likeGameService.deleteLikeGame(likeId, token);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
 }
