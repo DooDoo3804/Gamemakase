cb89a73f910a2c0037c811829b57008534857384
Bluuubery <zephyr72836@gmail.com>
2023-03-29 13:57:28 +0900

    ♻️ B.E fix : 이미 존재하는 게임 like시 기존 id 반환하도록 수정

--- backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/LikeGameRepository.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/model/repository/LikeGameRepository.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LikeGameRepository extends JpaRepository<LikeGame, Long> {
 	Page<LikeGame> findAllByUserOrderByLikeIdDesc(User user, Pageable page);
     List<LikeGame> findAllByUser(User user);
 
     boolean existsByGameGameIdAndUserUserId(Long gameId, Long userId);
+    Optional<LikeGame> findByGameGameIdAndUserUserId(Long gameId, Long userId);
 
 }
