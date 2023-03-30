436bd85696d40e6f93d22227ca05f53c7616c442
kwakminju <kacamimi@naver.com>
2023-03-30 16:18:36 +0900

    ✨ B.E feat : 검색 결과의 정렬을 정확도 순으로 변경합니다.

--- backend/gamemakase/src/main/java/com/gamemakase/domain/controller/ChatRestController.java
+++ backend/gamemakase/src/main/java/com/gamemakase/domain/controller/ChatRestController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RequiredArgsConstructor
 @RestController
 @Slf4j
