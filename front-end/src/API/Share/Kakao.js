a9e3efa232cf79f6b6ce742c140a37e88907b104
kwakminju <kacamimi@naver.com>
2023-04-03 13:05:33 +0900

    🚑 F.E Fix: 임시로 key값 직접 주입

--- front-end/src/API/Share/Kakao.js
+++ front-end/src/API/Share/Kakao.js
@@ -2,5 +2,6 @@ const {Kakao} = window;
 const APP_KEY = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY;
 
 export default function initialize() {
-    Kakao.init(APP_KEY);
+    console.log(APP_KEY);
+    Kakao.init("Ada2ce74b8235f51ccb9769d07b1a6d57");
 }
