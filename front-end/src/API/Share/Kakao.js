76cecdb5e70fea223bcaa5106667a09457600c0d
kwakminju <kacamimi@naver.com>
2023-04-03 13:34:00 +0900

    🚑 F.E Fix: docker compose 파일 수정

--- front-end/src/API/Share/Kakao.js
+++ front-end/src/API/Share/Kakao.js
@@ -2,5 +2,5 @@ const {Kakao} = window;
 const APP_KEY = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY;
 
 export default function initialize() {
-    Kakao.init(APP_KEY);
+    Kakao.init("Ada2ce74b8235f51ccb9769d07b1a6d57");
 }
